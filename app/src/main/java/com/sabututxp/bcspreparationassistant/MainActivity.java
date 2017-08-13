69deb5b6283d94d312dd2f48ff67fe4dff4195b5
sabu0050 <devilsworld27@gmail.com>
2017-08-13 15:29:16 +0600

    first commit

--- app/src/main/java/com/sabututxp/bcspreparationassistant/MainActivity.java
+++ app/src/main/java/com/sabututxp/bcspreparationassistant/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     Button subButton;
     private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
 
